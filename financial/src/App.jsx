08d47f212700d45078c6881cd8595c2fddffdf8b
rakibulislam161363 <rakibulislam161363@gmail.com>
2025-04-07 22:58:11 +0600

    all done

--- financial/src/App.jsx
+++ financial/src/App.jsx
@@ -12,7 +12,7 @@ import Rating from "./component/rating";
 function App() {
   
 
-  return (
+  return(
     <div className="bg-[#1A1E1C]">
       <div className="xl:w-[1440px] lg:mx-[100px] md:mx-[50px] mx-[25px]">
         <Navbar />
